9b877a32f29a75d069077c067b9bf624100247f8
Jiahe-J <jjh9401@qq.com>
2018-02-27 14:45:48 +0800

    数据表全部生成，migration目录&表django_migration。将app放到apps目录。

--- Mxonline3/settings.py
+++ Mxonline3/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+print(os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
