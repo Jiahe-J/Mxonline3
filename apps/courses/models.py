b63128294265d3dc99c34937d7d9d4c7bd77f3c9
Jiahe-J <jjh9401@qq.com>
2018-02-28 18:03:17 +0800

    安装xadmin，usersmodels三张表注册进xadmin, 配置搜索过滤展示字段，修复django2.0.1的DateField字段的indexError

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -32,6 +32,9 @@ class Course(models.Model):
         verbose_name = u"课程"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 # 章节
 class Lesson(models.Model):
@@ -45,6 +48,9 @@ class Lesson(models.Model):
         verbose_name = u"章节"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return '《{0}》课程的章节 >> {1}'.format(self.course, self.name)
+
 
 # 每章视频
 class Video(models.Model):
@@ -57,6 +63,9 @@ class Video(models.Model):
     class Meta:
         verbose_name = u"视频"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}章节的视频 >> {1}'.format(self.lesson, self.name)
 
 
 # 课程资源
@@ -76,3 +85,6 @@ class CourseResource(models.Model):
     class Meta:
         verbose_name = u"课程资源"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '《{0}》课程的资源: {1}'.format(self.course, self.name)
---
+++ apps/users/adminx.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+@author:silenthz 
+@file: adminx.py 
+@time: 2018/02/28 
+"""
+import xadmin
+from .models import EmailVerifyRecord, Banner
+
+
+# 创建admin的管理类,这里不再是继承admin，而是继承object
+class EmailVerifyRecordAdmin(object):
+    # 配置后台我们需要显示的列
+    list_display = ['email', 'code', 'send_type', 'send_time']
+    # 配置搜索字段,不做时间搜索
+    search_fields = ['code', 'email', 'send_type']
+    # 配置筛选字段
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+# 创建banner的管理类
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+# 将model与admin管理器进行关联注册
+xadmin.site.register(Banner, BannerAdmin)
