391ea6686769a49aeccfd1ea447a8b6731c70d5a
Jiahe-J <jjh9401@qq.com>
2018-03-05 00:01:06 +0800

    忘记密码重置功能实现完毕，并进行了必要的测试。

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -7,9 +7,11 @@
 """
 from random import Random
 
-from users.models import EmailVerifyRecord
+from django.template import loader
+
+from users.models import EmailVerifyRecord, UserProfile
 # 导入Django自带的邮件模块
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 # 导入setting中发送邮件的配置
 from Mxonline3.settings import EMAIL_FROM
 
@@ -27,7 +29,8 @@ def random_str(random_length=8):
 
 
 # 发送注册邮件
-def send_register_eamil(email, send_type="register"):
+def send_register_email(email, send_type="register"):
+    send_status = False
     # 发送之前先保存到数据库，到时候查询链接是否存在
 
     # 实例化一个EmailVerifyRecord对象
@@ -40,10 +43,6 @@ def send_register_eamil(email, send_type="register"):
 
     email_record.save()
 
-    # 定义邮件内容:
-    email_title = ""
-    email_body = ""
-
     if send_type == "register":
         email_title = "玉汝于成网站 注册激活链接"
         email_body = "请点击下面的链接激活你的账号: http://127.0.0.1:8000/active/{0}".format(code)
@@ -52,4 +51,28 @@ def send_register_eamil(email, send_type="register"):
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         # 如果发送成功
         if send_status:
-            pass
+            return "邮件发送成功"
+        else:
+            return "邮件发送失败，请确认邮箱地址是否正确"
+    elif send_type == "forget":
+
+        try:
+            user = UserProfile.objects.get(email=email)
+            if user:
+                email_title = "玉汝于成站 找回密码链接"
+                email_body = loader.render_to_string(
+                    "email_forget.html",  # 需要渲染的html模板
+                    {
+                        "active_code": code  # 参数
+                    }
+                )
+                msg = EmailMessage(email_title, email_body, EMAIL_FROM, [email])
+                msg.content_subtype = "html"
+                send_status = msg.send()
+        except Exception as e:
+            return str(e)
+    # 如果发送成功
+    if send_status:
+        return "success"
+    else:
+        return "fail"
