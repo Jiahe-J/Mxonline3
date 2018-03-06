311956d4acfb16b35065bdca8268162b5134ec40
Jiahe-J <jjh9401@qq.com>
2018-03-06 15:16:37 +0800

    完成机构列表根据类别、地区筛选，以及根据学习人数、课程数排序功能

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -46,6 +46,11 @@ class CourseOrg(models.Model):
     city = models.ForeignKey(CityDict, verbose_name=u"所在城市", on_delete=models.CASCADE)
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
+    # 当学生点击学习课程，找到所属机构，学习人数加1
+    students = models.IntegerField(default=0, verbose_name=u"学习人数")
+    # 当发布课程就加1
+    course_nums = models.IntegerField(default=0, verbose_name=u"课程数")
+
     class Meta:
         verbose_name = u"课程机构"
         verbose_name_plural = verbose_name
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -15,6 +15,28 @@ class OrgView(View):
         org_nums = all_orgs.count()
         # 取出所有的城市
         all_city = CityDict.objects.all()
+
+        # 取出筛选的城市
+        city_id = request.GET.get('city', '')
+        if city_id:
+            # 外键city_id在数据库中叫city_id
+            # 在机构中进一步筛选
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+        # 类别筛选
+        category = request.GET.get('ct', "")
+        if category:
+            # 我们就在机构中作进一步筛选类别
+            all_orgs = all_orgs.filter(category=category)
+        # 热门机构,如果不加负号会是有小到大。
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
+
+        # 进行排序
+        sort = request.GET.get('sort', "")
+        if sort:
+            if sort == "students":
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == "courses":
+                all_orgs = all_orgs.order_by("-course_nums")
         # 对课程机构进行分页
         # 尝试获取前台get请求传递过来的page参数
         # 如果是不合法的配置参数默认返回第一页
@@ -30,4 +52,9 @@ class OrgView(View):
             "all_orgs": orgs,
             "all_city": all_city,
             "org_nums": org_nums,
+            "city_id": city_id,
+            "category": category,
+            "hot_orgs": hot_orgs,
+            "sort": sort,
+
         })
