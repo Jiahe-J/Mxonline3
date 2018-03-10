8853d5c1a0ea5a13e1f96f9ff55fe9cacfe1821c
Jiahe-J <jjh9401@qq.com>
2018-03-10 13:26:56 +0800

    将机构详情展示完毕，为课程机构添加了收藏功能。

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -2,14 +2,17 @@
 from django.db import models
 from datetime import datetime
 
-
 # 课程信息表
+from organization.models import CourseOrg
+
+
 class Course(models.Model):
     DEGREE_CHOICES = (
         ("cj", u"初级"),
         ("zj", u"中级"),
         ("gj", u"高级")
     )
+    course_org = models.ForeignKey(CourseOrg, on_delete=models.CASCADE, verbose_name=u"所属机构", null=True, blank=True)
     name = models.CharField(max_length=50, verbose_name=u"课程名")
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     # TextField允许我们不输入长度。可以输入到无限大。暂时定义为TextFiled，之后更新为富文本
@@ -23,7 +26,7 @@ class Course(models.Model):
     image = models.ImageField(
         upload_to="courses/%Y/%m",
         verbose_name=u"封面图",
-        max_length=100)
+        max_length=100, null=True, blank=True)
     # 保存点击量，点进页面就算
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
