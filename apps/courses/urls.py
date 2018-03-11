fffb63876eaa2e3c3c5d12abc6442d21631e96f8
Jiahe-J <jjh9401@qq.com>
2018-03-11 16:14:04 +0800

    完成课程评论功能，添加评论并展示到页面。

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -5,7 +5,7 @@
 @file: urls.py 
 @time: 2018/03/10 
 """
-from courses.views import CourseListView, CourseDetailView, CourseInfoView
+from courses.views import CourseListView, CourseDetailView, CourseInfoView, CommentsView, AddCommentsView
 from django.urls import path, re_path
 
 app_name = "courses"
@@ -16,4 +16,8 @@ urlpatterns = [
     re_path('detail/(?P<course_id>\d+)/', CourseDetailView.as_view(), name="course_detail"),
     # 课程章节信息页
     re_path('info/(?P<course_id>\d+)/', CourseInfoView.as_view(), name="course_info"),
+    # 课程章节信息页
+    re_path('comments/(?P<course_id>\d+)/', CommentsView.as_view(), name="course_comments"),
+    # 添加课程评论,已经把参数放到post当中了
+    path('add_comment/', AddCommentsView.as_view(), name="add_comment"),
 ]
